238e5f1b61cbaa953bfbc08b14fcf663b6c076f1
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-11-21 09:47:34 +0100

    inital game start

--- src/components/BoxesContainer/index.tsx
+++ src/components/BoxesContainer/index.tsx
@@ -1,44 +1,25 @@
 import Box from "./components/Box";
-import { useThree } from "@react-three/fiber";
-import { useState } from "react";
-import * as THREE from "three";
-
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { initialStart } from "../../redux/slices/gameSlice";
 export default function BoxesContainer() {
-  useThree((state) => {
-    state.camera.lookAt(new THREE.Vector3(0, -0, -2));
-  });
-  const winPatterns = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
+  const dispatch = useDispatch();
 
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  const [players, setPlayers] = useState({
-    player1: {
-      playerName: "",
-      moves: [],
-    },
-    player2: {
-      playerName: "",
-      moves: [],
-    },
+  useEffect(() => {
+    dispatch(
+      initialStart({
+        player1Name: "player1",
+        player2Name: "player2",
+      })
+    );
   });
+
   return (
     <>
       <group
-        position={[0, -0, -5]}
+        position={[0, -1, -7.3]}
         rotation={[0, Math.PI / 2, (55 * Math.PI) / 180]}
       >
-        <mesh>
-          <boxGeometry attach="geometry" args={[9, 0.5, 9]} />
-          <meshBasicMaterial color={"#43a5d2"} />
-        </mesh>
         {/* row 1 */}
         <Box index={0} position={[2, 1, -3]} color="#b9b2d4" />
         <Box index={1} position={[2, 1, -3 + 3]} color="#fffef7" />
