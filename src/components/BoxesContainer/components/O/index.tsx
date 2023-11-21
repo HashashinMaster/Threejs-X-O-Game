402c9fb189a69d12b41100a3b15f8429dbdb5433
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-11-21 09:49:12 +0100

    marks animations

--- src/components/BoxesContainer/components/O/index.tsx
+++ src/components/BoxesContainer/components/O/index.tsx
@@ -1,10 +1,29 @@
 import { Euler } from "three";
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+export default function O({ selected }: { selected: boolean }) {
+  const oRef = useRef<THREE.Mesh>(null!);
 
-export default function O() {
+  useEffect(() => {
+    console.log(selected);
+    const tl = gsap.timeline({ repeat: -1, yoyo: true });
+    if (!selected) {
+      tl.to(oRef.current.position, { y: 3, duration: 0.4 });
+      // tl.to(oRef.current.position, { y: 2, duration: 0.4 });
+    } else {
+      tl.kill();
+      gsap.to(oRef.current.position, { y: 0.4, duration: 0.4 });
+    }
+  }, [selected]);
   return (
-    <mesh rotation={new Euler(0, Math.PI / 2, Math.PI / 2, "ZXY")}>
+    <mesh
+      ref={oRef}
+      position={[0, 2, 0]}
+      rotation={new Euler(0, Math.PI / 2, Math.PI / 2, "ZXY")}
+    >
       <torusGeometry args={[1, 0.3]} />
-      <meshBasicMaterial color={"#4fa0cc"} />
+      <meshBasicMaterial color={"#1296ee"} />
     </mesh>
   );
 }
--- src/components/BoxesContainer/components/X/index.tsx
+++ src/components/BoxesContainer/components/X/index.tsx
@@ -1,14 +1,28 @@
-export default function X() {
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+export default function X({ selected }: { selected: boolean }) {
+  const xRef = useRef<THREE.Group>(null!);
+  useEffect(() => {
+    const tl = gsap.timeline({ repeat: -1, yoyo: true });
+    if (!selected) {
+      tl.to(xRef.current.position, { y: 3, duration: 0.4 });
+      // tl.to(xRef.current.position, { y: 2, duration: 0.4 });
+    } else {
+      tl.kill();
+      gsap.to(xRef.current.position, { y: 0.4, duration: 0.4 });
+    }
+  }, [selected]);
   return (
-    <>
+    <group ref={xRef} position={[0, 2, 0]}>
       <mesh rotation={[0, (55 * Math.PI) / 180, 0]}>
         <boxGeometry args={[1, 0.5, 3]} />
-        <meshBasicMaterial color={"#f24b7a"} />
+        <meshBasicMaterial color={"#ec1652"} />
       </mesh>
       <mesh rotation={[0, -(55 * Math.PI) / 180, 0]}>
         <boxGeometry args={[1, 0.5, 3]} />
-        <meshBasicMaterial color={"#f24b7a"} />
+        <meshBasicMaterial color={"#ec1652"} />
       </mesh>
-    </>
+    </group>
   );
 }
