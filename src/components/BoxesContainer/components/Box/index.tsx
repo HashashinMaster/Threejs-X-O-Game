f62f6aeca146867b9f852a6d3c0748b5a17237a7
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-11-21 09:48:45 +0100

    saving players moves and handle marks for each one

--- src/components/BoxesContainer/components/Box/index.tsx
+++ src/components/BoxesContainer/components/Box/index.tsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
-import { Euler } from "three";
-
+import { useSelector, useDispatch } from "react-redux";
+import O from "../O";
+import X from "../X";
+import { RootState } from "../../../../redux/store";
+import {
+  addPlayerMove,
+  toggleCurrentPlayer,
+} from "../../../../redux/slices/gameSlice";
 type Props = {
   position: [x: number, y: number, z: number];
   color: string;
@@ -9,16 +15,45 @@ type Props = {
 export default function Box({ position, color, index }: Props) {
   const [show, setShow] = useState(false);
   const [selected, setSelected] = useState(false);
+  const [mark, setMark] = useState<JSX.Element | undefined>();
+  const { player1, currentPlayer } = useSelector(
+    (state: RootState) => state.gameReducer
+  );
+  const dispatch = useDispatch();
+  //   useEffect(() => {
+
+  //   },[])
   return (
     <group position={position}>
       <mesh
         onClick={() => {
-          if (!selected) setSelected(true);
-          else alert("pick another spot");
+          if (!selected) {
+            dispatch(
+              addPlayerMove({
+                player: currentPlayer.name as "player1" | "player2",
+                move: index,
+              })
+            );
+            console.log(currentPlayer, player1);
+            setMark(
+              currentPlayer === player1 ? (
+                <X selected={true} />
+              ) : (
+                <O selected={true} />
+              )
+            );
+            setSelected(true);
+
+            dispatch(toggleCurrentPlayer());
+          } else alert("pick another spot");
         }}
         onPointerEnter={() => {
-          setShow(true);
-          document.body.style.cursor = "pointer";
+          if (!selected) {
+            setShow(true);
+            document.body.style.cursor = "pointer";
+          } else {
+            document.body.style.cursor = "not-allowed";
+          }
         }}
         onPointerLeave={() => {
           setShow(false);
@@ -28,12 +63,16 @@ export default function Box({ position, color, index }: Props) {
         <boxGeometry attach="geometry" args={[3, 0.1, 3]} />
         <meshStandardMaterial color={color} />
       </mesh>
-      {(show || selected) && (
-        <mesh rotation={new Euler(0, Math.PI / 2, Math.PI / 2, "ZXY")}>
-          <torusGeometry args={[1, 0.3]} />
-          <meshStandardMaterial color={color} />
-        </mesh>
-      )}
+      {show &&
+        !selected &&
+        (currentPlayer === player1 ? (
+          <X selected={selected} />
+        ) : (
+          <O selected={selected} />
+        ))}
+      {selected && mark}
+
+      <meshStandardMaterial color={color} />
     </group>
   );
 }
