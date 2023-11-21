de2897fd20190e3c0da69cbeea76f116083f4161
EL MAHDI Bouzkoura <mehdibouzkoura1@gmail.com>
2023-11-21 13:09:08 +0100

    adding X animation

--- src/components/BoxesContainer/components/X/index.tsx
+++ src/components/BoxesContainer/components/X/index.tsx
@@ -1,8 +1,18 @@
 import gsap from "gsap";
 import { useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
 import * as THREE from "three";
-export default function X({ selected }: { selected: boolean }) {
+import { useFrame } from "@react-three/fiber";
+import { RootState } from "../../../../redux/store";
+export default function X({
+  selected,
+  index,
+}: {
+  selected: boolean;
+  index: number;
+}) {
   const xRef = useRef<THREE.Group>(null!);
+  const { winner } = useSelector((state: RootState) => state.gameReducer);
   useEffect(() => {
     const tl = gsap.timeline({ repeat: -1, yoyo: true });
     if (!selected) {
@@ -13,6 +23,33 @@ export default function X({ selected }: { selected: boolean }) {
       gsap.to(xRef.current.position, { y: 0.4, duration: 0.4 });
     }
   }, [selected]);
+
+  useEffect(() => {
+    console.log("im here");
+    if (
+      winner && winner.winPattern && winner.winPattern.includes(index)
+        ? true
+        : false
+    ) {
+      console.log("should animate");
+      const tl2 = gsap.timeline();
+      tl2.to(xRef.current.position, {
+        y: 3,
+        duration: 0.4,
+      });
+    }
+  }, [winner.player]);
+
+  useFrame(() => {
+    if (
+      winner && winner.winPattern && winner.winPattern.includes(index)
+        ? true
+        : false
+    ) {
+      xRef.current.rotation.z += 0.1;
+      // console.log(xRef.current.rotation.z);
+    }
+  });
   return (
     <group ref={xRef} position={[0, 2, 0]}>
       <mesh rotation={[0, (55 * Math.PI) / 180, 0]}>
